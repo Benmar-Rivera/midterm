2e23b689ece43ff1cc375ccd4456b099536f9bb1
Benmar-Rivera <benmar.rivera@cvsu.edu.ph>
2025-10-27 13:03:56 +0800

    commit

--- Project/app/views.py
+++ Project/app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Post
 
 class HomePageView(TemplateView):   
     template_name = 'app/home.html'
 
 class AboutPageView(TemplateView):   
     template_name = 'app/about.html'
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'app/blog_list.html'
